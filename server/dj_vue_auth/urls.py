0cfe7443f1262ba5bf3009905aa81aa6095b3dee
Hamdi Guenichi <guenichihamdi1@gmail.com>
2021-08-31 23:11:40 +0100

    password_reset

--- server/dj_vue_auth/urls.py
+++ server/dj_vue_auth/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework import routers
 from django.conf import settings
 from django.conf.urls.static import static
+from dj_rest_auth.registration.views import VerifyEmailView
+
 
 from users.views import UserViewSet
 
@@ -10,8 +12,10 @@ router = routers.DefaultRouter()
 router.register(r'users', UserViewSet) 
 
 urlpatterns = [
+    path(r'^', include('django.contrib.auth.urls')),
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('dj-rest-auth/registration/account-confirm-email/', VerifyEmailView.as_view(), name='account_email_verification_sent')
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
